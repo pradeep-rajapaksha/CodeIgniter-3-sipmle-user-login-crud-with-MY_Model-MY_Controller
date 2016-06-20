01c43b7aa5aa9b452e7c66520024e4cb6d296ffd
Pradeep Rajapaksha <pradeepprasanna.rajapaksha4@gmail.com>
2016-06-20 16:21:39 +0530

    Update MY_Controller.php

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -12,12 +12,6 @@
  * programmer can easily call methods in My_Model Class for all most
  * all Database Communication and minimize the coding in their projects.
  *
- * @author     Dharshana Jayamaha <dharshana@openarc.lk>
- * @copyright  2013 RAD Team - OpenArc
- * @license    http://www.php.net/license/3_01.txt  PHP License 3.01
- * @version    Release: 1.0
- * @link       http://pear.php.net/package/PackageName
- * @since      Class available since Release 1.0.0
  */
 class MY_Controller extends CI_Controller
 {
